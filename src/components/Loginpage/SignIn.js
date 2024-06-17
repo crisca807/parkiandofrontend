beb0bd712dfbc7a4bd04d04adc3f7bc4a165e18f
TheJorghys <128424954+TheJorghys@users.noreply.github.com>
2024-06-16 21:33:02 -0500

    finish one

--- src/components/Loginpage/SignIn.js
+++ src/components/Loginpage/SignIn.js
@@ -3,8 +3,6 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -17,77 +15,74 @@ import '../../assets/css/Login_Styles.css'; // Importa los estilos CSS
 function SignIn() {
   return (
     <div className='body-login'>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className="paper">
-        <Avatar className="avatar">
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Iniciar sesión
-        </Typography>
-        <form className="form" noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="correo-electronico"
-            label="Correo electrónico"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Contraseña"
-            type="password"
-            id="contraseña"
-            autoComplete="current-password"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-          <Button
-            component={RouterLink}
-            to="/"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className="submit"
-          >
-            Sign In
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2">
-                Forgot password?
-              </Link>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className="paper">
+          <Avatar className="avatar">
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Iniciar sesión
+          </Typography>
+          <form className="form" noValidate>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="correo-electronico"
+              label="Correo electrónico"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              className="textField"
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Contraseña"
+              type="password"
+              id="contraseña"
+              autoComplete="current-password"
+              className="textField"
+            />
+            <Button
+              component={RouterLink}
+              to="/"
+              fullWidth
+              variant="contained"
+              className="submit"
+            >
+              Iniciar sesión
+            </Button>
+            <Grid container className="links-container">
+              <Grid item xs={12}>
+                <Link href="#" variant="body2" className="forgot-password">
+                  ¿Olvidaste tu contraseña?
+                </Link>
+              </Grid>
+              <Grid item xs={12}>
+                <RouterLink to="/registerOptions" variant="body2" className="sign-up-link">
+                  {"¿No tienes una cuenta? Regístrate"}
+                </RouterLink>
+              </Grid>
             </Grid>
-            <Grid item>
-              <RouterLink to="/user_register" variant="body2">
-                {"Don't have an account? Sign Up"}
-              </RouterLink>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
-        <Typography variant="body2" color="textSecondary" align="center">
-          {'Copyright © '}
-          <Link color="inherit" href="https://mui.com/">
-            Your Website
-          </Link>{' '}
-          {new Date().getFullYear()}
-          {'.'}
-        </Typography>
-      </Box>
-    </Container>
+          </form>
+        </div>
+        <Box mt={8}>
+          <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="#">
+              Parkiando
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+          </Typography>
+        </Box>
+      </Container>
     </div>
   );
 }
