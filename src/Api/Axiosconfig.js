a4c5b50891dbb88f48282f2029e2eb3880c5bcdf
crisca807 <crisca807@gmail.com>
2024-06-27 18:59:50 -0500

    se edito conexiones

--- src/Api/Axiosconfig.js
+++ src/Api/Axiosconfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:10000',
+    baseURL: 'https://api-rest-node-js-parkiando.onrender.com',
     headers: {
         'Content-Type': 'application/json',
     }
